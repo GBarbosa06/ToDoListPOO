d92f0892a8ea9abaa4af511b5467be9086478883
gbarbosa06 <andradegui2006@gmail.com>
2025-09-02 21:14:51 -0300

    Model Tarefa feito

---
+++ src/Tarefa.java
@@ -0,0 +1,71 @@
+import java.time.LocalDateTime;
+
+public class Tarefa {
+    private int id;
+    private String titulo;
+    private String descricao;
+    private boolean completa;
+    private LocalDateTime dataAgora;
+
+    // Construtores (recursivos, não está na descrição da atividade, mas foi solicitado na aula)
+    public Tarefa(){
+        this("Sem título");
+    }
+
+    public Tarefa(String titulo){
+        this.titulo = titulo;
+        this.dataAgora = LocalDateTime.now();
+        this.completa = false;
+    }
+
+    public Tarefa(String titulo, String descricao) {
+        this(titulo);
+        this.descricao = descricao;
+    }
+
+    public Tarefa(String titulo, String descricao, boolean completa) {
+        this(titulo, descricao);
+        this.completa = completa;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getDataAgora() {
+        return dataAgora;
+    }
+
+    public void setDataAgora(LocalDateTime dataAgora) {
+        this.dataAgora = dataAgora;
+    }
+
+    public boolean isCompleta() {
+        return completa;
+    }
+
+    public void setCompleta(boolean completa) {
+        this.completa = completa;
+    }
+}
